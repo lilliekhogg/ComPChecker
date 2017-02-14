1fb3be72d7a67c827d6663288aa230b73ea30298
lilliekhogg <lilliekhogg@gmail.com>
2017-02-14 15:01:43 +0000

    Storage

---
+++ src/Storage.java
@@ -0,0 +1,104 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Storage {
+    
+    String make;
+    String model;
+    double price;
+    String series;
+    boolean HHD;
+    int speed;
+    int capacityGB;
+  
+    
+    
+ public void setMake (String make){
+    this.make = make;
+ }
+ 
+ public void setModel (String model){
+     this.model = model;
+ }
+ 
+ public void setPrice (double price){
+     this.price = price;
+ }
+ 
+ public void setSeries (String series){
+     this.series = series;
+ }
+ 
+ public void setHHD (boolean HHD){
+     this.HHD = HHD;
+ }
+ 
+ public void setSpeed (int speed){
+     this.speed = speed;
+ }
+ 
+ public void setCapacityGB (int capacityGB){
+     this.capacityGB = capacityGB;
+ }
+ 
+ 
+  public boolean saveStorage() {
+
+        Connection con = DatabaseConnection.establishConnection();
+
+        try {
+            String query = "INSERT INTO Part (Price,Model,Make,PartType) VALUES (?,?,?,?)";
+
+            PreparedStatement statement = con.prepareStatement(query);
+
+            statement.setDouble(1, this.price);
+            statement.setString(2, this.model);
+            statement.setString(3, this.make);
+            statement.setString(4, "Storage");
+            statement.execute();
+            String model = this.model;
+            query = "SELECT * FROM Part WHERE Model ='" + model + "' && PartType = 'Storage'";
+
+            statement.executeQuery(query);
+            ResultSet rs = statement.getResultSet();
+            int partID = 0;
+            while (rs.next()) {
+                partID = rs.getInt("PartID");
+            }
+
+            query = "INSERT INTO Storage values (?,?,?,?,?)";
+
+            statement = con.prepareStatement(query);
+
+            statement.setInt(1, partID);
+            statement.setString(2, this.series);
+            statement.setBoolean(3, this.HHD);
+            statement.setInt(4, this.speed);
+            statement.setInt(5, this.capacityGB);
+            statement.execute();
+
+
+            return true;
+
+        } catch (SQLException err) {
+            return false;
+
+        }
+
+ 
+ 
+  } 
+}
